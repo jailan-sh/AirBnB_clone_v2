e7cde89d6a6bead09db8a89a83f1adf26c5afc49
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-19 22:19:31 +0300

    relationship added

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -10,4 +11,4 @@ class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = column(string(128), nullable=False)
-    cities = relationship(City, cascade="all, delete_oriphan", backref="state")
+    cities = relationship(City, cascade="all, delete_orphan", backref="state")
