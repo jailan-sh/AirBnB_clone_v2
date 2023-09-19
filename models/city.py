2943002351af3e34bfc386f0b4ae5bc236c40bc0
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-19 13:25:29 -0700

    import edit

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
