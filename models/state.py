5ff04e51ab62f9bbabae53f074acfae7f1afc9fd
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-19 21:45:06 +0300

    update models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = column(string(128), nullable=False)
+    cities = relationship(City, cascade="all, delete_oriphan", backref="state")
