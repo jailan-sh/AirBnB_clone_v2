898d7fa114826d705002fc9dffa0f95ea5979c79
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-20 02:38:46 +0300

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
+
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.base_model import BaseModel, Base
 import os
 
-engine = create_engine(self.__engine)
-class DBStorage(Base):
+
+class DBStorage:
     """ this class manage the db storage for hbnb clone"""
     __engine = None
     __session = None
+
     def __init__(self):
-        """ init method"""
+        """constructor for DBstorage class"""
         user =  os.environ.get('HBNB_MYSQL_USER')
         password = os.environ.get('HBNB_MYSQL_PWD')
-        host =  os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+        host =  os.environ.get('HBNB_MYSQL_HOST')
         database = os.environ.get('HBNB_MYSQL_DB')
         
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                               .format(user, password, host, database),
-                               pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(user, password, host, database),
+                                      pool_pre_ping=True)
         if os.environ.get('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
         else:
