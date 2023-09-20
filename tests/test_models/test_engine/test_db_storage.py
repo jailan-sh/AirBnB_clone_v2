52a6638560e83d957411cd692079a665690b37db
ghaidaareda <dodo.sa7afy@gmail.com>
2023-09-21 00:35:20 +0300

    changes

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module to test console 
+"""
+
+import unittest
+from models.engine.file_storage import DBStorage
+import os
+
+
+class TestDBstorage(unittest.TestCase):
+    """
+    test dbstorage 
+    """
+    @unittest.skipIF(os.getenv("HBNB_TYPE_STORAGE") != "db")
+    def test_console_documentation(self):
+        """ test db docs"""
+        self.assertIsNOTNone(DBStorage.__doc__)
