35dad0719a13a2cbe2784a280e0213c69630ef92
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-23 01:38:50 +0300

    10 still

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Integer, Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = 
