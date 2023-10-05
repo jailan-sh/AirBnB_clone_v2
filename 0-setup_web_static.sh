eacbcbb75136d0f1969e2ebeae04772568a5d0a8
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-05 12:14:19 -0700

    edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,33 @@
-#!usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+#!/usr/bin/env bash
+# Sets up a web server for deployment of a static website:
 
-sudo apt-get -y update
-sudo apt-get -y nginx
+if ! which nginx > /dev/null 2>&1;
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+    sudo ufw allow 'Nginx HTTP'
+fi
+if [[ ! -e /data/web_static/releases/test ]];
+then
+    sudo mkdir -p /data/web_static/releases/test
+fi
 
-mkdir -p /data/web_static/releases/test/ 
-mkdir -p /data/web_static/shared/
+if [[ ! -e /data/web_static/shared ]];
+then
+    sudo mkdir -p /data/web_static/shared
+fi
 
-echo "Hello world" >> /data/web_static/releases/test/index.html
+sudo ln -sfn /data/web_static/releases/test /data/web_static/current
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current 
+echo "<html>
+  <head>
+  </head>
+  <body>
+    hello world
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+chown -R ubuntu:ubuntu /data/
 
 echo "
 server {
@@ -18,12 +36,13 @@ server {
 
         location /hbnb_static {
             alias /data/web_static/current/;
-            autoindex off;
         }
+
+        add_header X-Served-By $HOSTNAME;
+
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1\wu4 \
+permanent;
 }" > /etc/nginx/sites-available/default
 
-sudo chown -R ubuntu:ubuntu /data/
-
-service nginx restart
-
+sudo service nginx restart
 exit 0
