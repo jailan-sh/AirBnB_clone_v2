f3c2bf9872e8465a0752fad5946273e471515e37
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-06 01:48:03 +0300

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a Fabric script that generates a .tgz archive """
+from datetime import datetime
+import os
+from fabric.api import local
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents of the web_static folder
+    """
+    dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    file = "versions/web_static_{}.tgz".format(dt)
+    if os.path.isdir(versions) is False:
+        local('mkdir versions')
+    if local('tar -czvf {} web_static'.format(file)).failed is True :
+        return None
+    return file
