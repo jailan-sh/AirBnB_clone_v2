6a02fb38ff28d758ab1ca5d45ef71b5d458f9338
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-06 03:30:32 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,7 +11,18 @@ def do_deploy(archive_path):
     Fabric script that distributes an archive to your web servers
     """
     if os.path.isfile(archive_path) is False:
+            return False
+    try:
+        file = archive_path.split('/')[-1].split('.')[0]
+        zipt = archive_path.split('/')[-1]
+        path = "/data/web_static/releases/{}".format(file)
+        
+        put(archive_path, '/tmp/')
+        run("mkdir {}".format(path))
+        run('tar -xzf /tmp/{} -c {}'.format(zipt, path))
+        run('rm /tmp/{}'.format(zipt))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path))
+        return True
+    except:
         return False
-    
-    
-    
