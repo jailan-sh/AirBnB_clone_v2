c341bec63a751b5c2c824efb0199f63c5812b43e
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-06 03:59:44 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,17 +6,18 @@ from fabric.api import *
 
 env.hosts = ['54.196.48.32', '34.229.255.108']
 
+
 def do_deploy(archive_path):
     """
     Fabric script that distributes an archive to your web servers
     """
     if os.path.isfile(archive_path) is False:
-            return False
+        return False
     try:
         file = archive_path.split('/')[-1].split('.')[0]
         zipt = archive_path.split('/')[-1]
         path = "/data/web_static/releases/{}".format(file)
-        
+
         put(archive_path, '/tmp/')
         run("mkdir {}".format(path))
         run('tar -xzf /tmp/{} -C {}'.format(zipt, path))
