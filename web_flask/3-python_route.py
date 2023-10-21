b77f93cfd434687dc5003d23fc6677864b1ddd77
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-21 17:37:06 +0300

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" start web flASK"""
+
+
+from flask import Flask
+
+
+airbnb = Flask(__name__)
+
+
+@airbnb.route("/", strict_slashes=False)
+def hello():
+    """display hello"""
+    return "Hello HBNB!"
+
+
+@airbnb.route("/hbnb", strict_slashes=False)
+def about_hbnb():
+    """display hbnb"""
+    return "HBNB"
+
+
+@airbnb.route("/c/<text>", strict_slashes=False)
+def fun_c(text):
+    """display message pass when c is called"""
+    return "C " + text.replace("_", " ")
+
+
+@airbnb.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
+@airbnb.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """display message pass when python is called"""
+    return "Python "+ text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    airbnb.run(host="0.0.0.0", port=5000, debug=True)
