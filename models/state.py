4ebfd285aaa91598d6ccb47e0cdf31df8a7fce36
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-22 16:41:54 +0300

    7

--- models/state.py
+++ models/state.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
 import models
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
         cities = relationship("City",
                               backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-        name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """Return the list of City instances corresponding to this state"""
-            my_cities = models.storage.all(City)
-            state_city = [cc for cc in my_cities.values()
-                          if cc.state_id == self.id]
-            return state_city
+            """ Returns the list of City instances with
+            state_id == current State.id """
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city_ins in all_cities.values():
+                if city_ins.state_id == self.id:
+                    state_cities.append(city_ins)
+
+            return state_cities
