98bdc98871611dd3ee52ad7bd3cebae2426ff58b
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-22 13:53:44 +0300

    7

--- models/state.py
+++ models/state.py
@@ -2,25 +2,33 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String, Integer, MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import environ
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """returns the list of City instances
-            with state_id equals to the current State.id"""
-            cities_list = []
-            for city in storage.all(City).values():
-                if city.state_id == Self.id:
-                    cities_list.append(city)
-            return cities_list
+            """Return the list of City instances corresponding to this state"""
+            my_cities = models.storage.all(City)
+            state_city = [cc for cc in my_cities.values()
+                          if cc.state_id == self.id]
+            return state_city
