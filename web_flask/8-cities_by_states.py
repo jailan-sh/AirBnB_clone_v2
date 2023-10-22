4abe067e6357ecebe251e8cb330779736b8d6ab7
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-22 22:34:37 +0300

    9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,5 +14,11 @@ def cities_list():
     return render_template('8-cities_by_states.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove SQLAlchemy Session """
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
