eea36f52c4e8696bf14b6447ab813dd43429ad26
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-22 20:10:21 +0300

    8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """ start web flASK"""
 from models import storage
+from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardwon(self):
-    """ remove the current SQLAlchemy Session"""
+def close_db(self):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def satatic_list():
-    """display a HTML page"""
-    states = storage.all('State').values()
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
